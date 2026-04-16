255a37a2d69110d247d2dafac6fd85a7cb8bd5de
Jhonatan Oliveira <oliveirajhonatanjesus18@gmail.com>
2026-04-16 16:59:56 -0300

    Inicializando meu projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {	// Método principal que executa o código
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,24 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// Variáveis 
+		
+		int idade = 25;
+		String nome = "Jhonatan";
+		float graus = 35.0f;  //ponto flutuante
+		double graus2 = 34.0; //ponto flutuante
+		boolean resposta = true; // logico true/false
+		
+		
+		// Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.println(nome);
+		System.out.print(QUANTIDADE);
+	}
+
+}
